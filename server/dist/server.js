b113e9ab7a19480d6eef3193094b67b989523960
aravindanaronraj <aravindanaronraj@gmail.com>
2026-09-07 07:37:29 +0530

    restart again

--- server/dist/server.js
+++ server/dist/server.js
@@ -6,11 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const app_1 = __importDefault(require("./app"));
 const db_1 = __importDefault(require("./config/db"));
-const seedTamilBooks_1 = require("./scripts/seedTamilBooks");
 const PORT = process.env.PORT || 5000;
 const startServer = async () => {
     await (0, db_1.default)();
-    await (0, seedTamilBooks_1.seedAllDefaults)();
     app_1.default.listen(PORT, () => {
         console.log(`🚀 Server is running on http://localhost:${PORT}`);
     });
